254c12d27429d021ae63590596e5598d83e33e07
LucasS <lucasmsoliman@gmail.com>
2024-11-21 10:17:53 -0300

    adicionando a sala de cada membro

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,7 +1,5 @@
 import styles from './Members.module.css';
 
-
-
 const Members = () => {
     return (
         <div className={styles.container}>
@@ -11,23 +9,25 @@ const Members = () => {
                     <img src="/images/lucasH.jpg" alt="Lucas" className={styles.memberPhoto} />
                     <p className={styles.memberName}>Lucas Henrique de Souza Santos</p>
                     <p className={styles.memberRM}>RM558241</p>
-                    <a href="www.linkedin.com/in/
-                    lucas-h-9694582b6" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
+                    <p className={styles.memberClass}>Turma: 1TDSPI</p> 
+                    <a href="https://www.linkedin.com/in/lucas-h-9694582b6" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
                     <a href="https://github.com/membro1" target="_blank" rel="noopener noreferrer" className={styles.link}>GitHub</a>
                 </div>
 
                 <div className={styles.memberCard}>
                     <img src="/images/lucas.jpg" alt="Lucas S" className={styles.memberPhoto} />
-                    <p className={styles.memberName}>Lucas Martins Soliman </p>
+                    <p className={styles.memberName}>Lucas Martins Soliman</p>
                     <p className={styles.memberRM}>RM558506</p>
+                    <p className={styles.memberClass}>Turma: 1TDSR</p> 
                     <a href="https://www.linkedin.com/in/lucassoliman/" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
                     <a href="https://github.com/soliyy" target="_blank" rel="noopener noreferrer" className={styles.link}>GitHub</a>
                 </div>
 
                 <div className={styles.memberCard}>
-                    <img src="/images/ryan.jpg" alt="ryan" className={styles.memberPhoto} />
+                    <img src="/images/ryan.jpg" alt="Ryan" className={styles.memberPhoto} />
                     <p className={styles.memberName}>Ryan Fernando Lucio da Silva</p>
                     <p className={styles.memberRM}>RM555924</p>
+                    <p className={styles.memberClass}>Turma: 1TDSPI</p> 
                     <a href="https://www.linkedin.com/in/ryan-fernando-028b94333?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
                     <a href="https://github.com/ryanxaxxxx" target="_blank" rel="noopener noreferrer" className={styles.link}>GitHub</a>
                 </div>
